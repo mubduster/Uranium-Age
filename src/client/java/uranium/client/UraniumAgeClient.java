5f6bbc90761adc7fa04208a0e97d20da6112754f
mubduster <mubassirmuneeb51@gmail.com>
2026-05-20 22:09:46 +0500

    initial

---
+++ src/client/java/uranium/client/UraniumAgeClient.java
@@ -0,0 +1,10 @@
+package uranium.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UraniumAgeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
